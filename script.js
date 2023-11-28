b31e4c654e31227af8c9c57bd58b39160575180e
maria eduarda mendes <110263250+mendeseduarda@users.noreply.github.com>
2023-11-28 08:43:39 -0300

    aula1

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var personagens = document.getElementById("personagens");
+    var submenu = personagens.querySelector(".submenu");
+
+    personagens.addEventListener("click", function (event) {
+        event.stopPropagation(); 
+
+        if (submenu.style.display === "block") {
+            submenu.style.display = "none";
+        } else {
+            submenu.style.display = "block";
+        }
+    });
+
+    document.addEventListener("click", function () {
+        submenu.style.display = "none";
+    });
+});
